e51738427b63e1d2a4d126830571c82734549217
Joyce <joycewachira718@gmail.com>
2023-12-30 17:56:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+
+   }
+
+
+   switch (feet) {
+    case 2001:
+      return 'I will gladly take your thirty bucks.'
+       
+    case 2501:
+
+       return 'No can do.';
+       default:
+      break; 
+
+  // Write your code here!
+}
+}
+
+function ternaryCheckCity(city) {
+
+let visit = city === "NYC" ? "Ok, sounds good.": "No go." ;
+return visit;
+
+
+
+
+  // if (city === "NYC") {
+  //   return "Ok, sounds good.";
+  // } else if (city != "NYC") {
+  //   return "No go.";
+  // }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  // Write your code
+
+switch (tip) {
+  case "generous":
+    return "Thank you so much.";
+    
+  case "not as generous":
+    return "Thank you.";
+
+  default:
+    return "Bye.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
